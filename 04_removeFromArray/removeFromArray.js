46fca2c598be85ba68571f64492040989382a2ef
CookieJari <zalmanhajari@gmail.com>
2022-11-03 18:30:54 +0800

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+function removeFromArray(arr, par){
+    
+        
+    for (let a = 0; a < arguments.length; a++) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i]===arguments[a]){
+                arr.splice(i,1);
+            } 
+        }
+             
+    }
 
-};
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
