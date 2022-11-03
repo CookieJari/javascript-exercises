e7966c77beabc773260019297b2b4bce01cf2c2a
CookieJari <zalmanhajari@gmail.com>
2022-11-03 19:06:39 +0800

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+function sumAll(n1,n2){
+    let output=0;
 
-};
+        if (n1>0 && n2>0 && typeof n1==='number' && typeof n2==='number') {
+            if (n2<n1) {
+                let temp=0;
+                n2=temp;
+                n2=n1;
+                n1=temp;
+            }
+            for (n1; n1 <= n2; n1++) {
+                output+=n1;
+                
+            }
+            return output;
+        } 
+        return 'ERROR'
+    
+    
+}
 
 // Do not edit below this line
 module.exports = sumAll;
