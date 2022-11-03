ace5649835158c4690a52bf014770f4b35bc215d
CookieJari <zalmanhajari@gmail.com>
2022-11-03 17:52:15 +0800

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
-
+function reverseString(str){
+    output="";
+    for (let i = str.length-1; i >= 0; i--) {
+        output += str[i];
+        
+    }
+    return output;
+}
 // Do not edit below this line
 module.exports = reverseString;
