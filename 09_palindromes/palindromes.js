293181150ba631c29b1030169f3eb46d1825bc4e
CookieJari <zalmanhajari@gmail.com>
2023-03-12 21:31:26 +0800

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    
+    var regPat=/[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(regPat,"");
+    return str == str.toLowerCase().replace(regPat,"").split("").reverse().join("");
 };
 
 // Do not edit below this line
