c4dbb50b093ee9450797ffbaab707643b0c6405b
CookieJari <zalmanhajari@gmail.com>
2023-03-12 23:27:29 +0800

    yey finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    var prev =1;
+    var cur =1;
+    if (n<0) {
+        return "OOPS";
+    }
+
+    for (let index = 2; index < n; index++) {
+        var temp = cur;
+        cur = prev+cur;
+        prev = temp; 
+    }
+
+    return cur;
 
 };
 
