bd2ec025997f8a48efbb031d4dcae61564cdf35c
CookieJari <zalmanhajari@gmail.com>
2023-03-13 01:47:30 +0800

    find the oldest is stupid remember do not code at night too much

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    return people.reduce(function(oldest, person){ 
+        var oldestAge = GetAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        var personAge = GetAge(person.yearOfBirth, person.yearOfDeath);
+        return oldestAge > personAge ? oldest : person;
+    })
+};
 
+const GetAge = function(birth,death){
+    if (!death) {
+        return new Date().getFullYear()-birth;
+    }
+    return death-birth;
 };
 
 // Do not edit below this line
